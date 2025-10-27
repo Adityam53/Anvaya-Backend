c90d5fb81982feb9a4c9943a64689a1767103a93
Aditya Moorjmalani <adityamoorjmalani@Adityas-MacBook-Air.local>
2025-10-27 16:14:25 +0530

    First commit: Done with db models and their relationships.

---
+++ Models/comment.models.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  lead: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Lead",
+    requirec: [true, "Lead reference is required."],
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SalesAgent",
+    requirec: [true, "Author reference is required."],
+  },
+  commentText: {
+    type: String,
+    required: [true, "Comment text is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
